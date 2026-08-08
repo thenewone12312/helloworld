63542129d75e5f4ee1de69b7fdf4ad066885311a
VictoryDance <victoralexpapa@gmail.com>
2026-08-07 22:02:21 -0400

    matrix (incomplete)

--- include/Texture.h
+++ include/Texture.h
@@ -1,5 +1,20 @@
 #pragma once
 
-class Texture{
+#include <string>
+#include <iostream>
+#include <glad/glad.h>
 
+#include <stb/image.h>
+
+class Texture {
+public:
+    Texture(const std::string& path);
+    ~Texture();
+    
+    GLuint getID() const;
+
+private:
+    int width, height, channels;
+    unsigned char* data;
+    GLuint ID;
 };
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -2,45 +2,57 @@
 
 Texture::Texture(const std::string& path)
 {
-    int width, height, channels;
-
-    unsigned char* data = stbi_load(
-        path.c_str(),
-        &width,
-        &height,
-        &channels,
-        0
-    );
-
-    if (!data) {
-        std::cout << "Failed to load texture: " << path << std::endl;
-        ID = 0;
-        return;
-    }
 
     glGenTextures(1, &ID);
+    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, ID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // int width, height, channels;
 
-    GLenum format = channels == 4 ? GL_RGBA : GL_RGB;
+    // unsigned char* data = stbi_load(
+    //     path.c_str(),
+    //     &width,
+    //     &height,
+    //     &channels,
+    //     0
+    // );
 
-    glTexImage2D(
-        GL_TEXTURE_2D,
-        0,
-        format,
-        width,
-        height,
-        0,
-        format,
-        GL_UNSIGNED_BYTE,
-        data
-    );
+    // if (!data) {
+    //     std::cout << "Failed to load texture: " << path << std::endl;
+    //     ID = 0;
+    //     return;
+    // }
 
-    glGenerateMipmap(GL_TEXTURE_2D);
+    // glGenTextures(1, &ID);
+    // glBindTexture(GL_TEXTURE_2D, ID);
 
-    stbi_image_free(data);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // GLenum format = channels == 4 ? GL_RGBA : GL_RGB;
+
+    // glTexImage2D(
+    //     GL_TEXTURE_2D,
+    //     0,
+    //     format,
+    //     width,
+    //     height,
+    //     0,
+    //     format,
+    //     GL_UNSIGNED_BYTE,
+    //     data
+    // );
+
+    // glGenerateMipmap(GL_TEXTURE_2D);
+
+    // stbi_image_free(data);
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &ID);
 }
